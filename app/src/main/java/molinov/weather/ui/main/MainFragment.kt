698cbb08863ec07b05d0d55f45d6d2db2945e6ec
Molinov <molinov@list.ru>
2021-05-16 13:36:26 +0300

    AppState

--- app/src/main/java/molinov/weather/ui/main/MainFragment.kt
+++ app/src/main/java/molinov/weather/ui/main/MainFragment.kt
@@ -6,7 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import molinov.weather.R
+import androidx.lifecycle.Observer
+import com.google.android.material.snackbar.Snackbar
+import molinov.weather.AppState
+import molinov.weather.databinding.MainFragmentBinding
 
 class MainFragment : Fragment() {
 
@@ -14,17 +17,48 @@ class MainFragment : Fragment() {
         fun newInstance() = MainFragment()
     }
 
+    private var _binding: MainFragmentBinding? = null
+    private val binding get() = _binding!!
     private lateinit var viewModel: MainViewModel
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = MainFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+        viewModel.getLiveData().observe(viewLifecycleOwner, Observer { renderData(it) })
+        viewModel.getWeather()
     }
 
+    private fun renderData(appState: AppState) {
+        when (appState) {
+            is AppState.Success -> {
+                val weatherData = appState.weatherData
+                binding.loadingLayout.visibility = View.GONE
+                Snackbar.make(binding.mainView, "Success", Snackbar.LENGTH_LONG).show()
+            }
+            is AppState.Loading -> {
+                binding.loadingLayout.visibility = View.VISIBLE
+            }
+            is AppState.Error -> {
+                binding.loadingLayout.visibility = View.GONE
+                Snackbar
+                    .make(binding.mainView, "Error", Snackbar.LENGTH_INDEFINITE)
+                    .setAction("Reload") { viewModel.getWeather() }
+                    .show()
+            }
+        }
+    }
 }
--- app/src/main/java/molinov/weather/ui/main/MainViewModel.kt
+++ app/src/main/java/molinov/weather/ui/main/MainViewModel.kt
@@ -1,7 +1,23 @@
 package molinov.weather.ui.main
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import molinov.weather.AppState
+import java.lang.Thread.sleep
 
-class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class MainViewModel(
+    private val liveDataToObserve: MutableLiveData<AppState> = MutableLiveData()
+) : ViewModel() {
+
+    fun getLiveData() = liveDataToObserve
+
+    fun getWeather() = getDataFromLocalSource()
+
+    private fun getDataFromLocalSource() {
+        liveDataToObserve.value = AppState.Loading
+        Thread {
+            sleep(1000)
+            liveDataToObserve.postValue(AppState.Success(Any()))
+        }.start()
+    }
 }
