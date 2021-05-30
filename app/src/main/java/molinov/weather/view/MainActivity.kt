42dcbc3458c6cac11b38885836b6b20a39f083a8
VMolinov <molinov@list.ru>
2021-05-30 19:57:03 +0300

    onReceive fail

--- app/src/main/java/molinov/weather/view/MainActivity.kt
+++ app/src/main/java/molinov/weather/view/MainActivity.kt
@@ -1,81 +1,59 @@
 package molinov.weather.view
 
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Build
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.util.Log
-import android.view.View
+import android.view.Menu
+import android.view.MenuItem
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import molinov.weather.R
 import molinov.weather.databinding.MainActivityBinding
-import molinov.weather.databinding.MainActivityWebviewBinding
+import molinov.weather.view.experiments.MainBroadcastReceiver
 import molinov.weather.view.main.MainFragment
-import java.io.BufferedReader
-import java.io.InputStreamReader
-import java.net.MalformedURLException
-import java.net.URL
-import java.util.stream.Collectors
-import javax.net.ssl.HttpsURLConnection
+import molinov.weather.view.experiments.ThreadsFragment
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: MainActivityBinding
+    private val receiver = MainBroadcastReceiver()
 
     @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = MainActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
-//        binding.ok.setOnClickListener(clickListener)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
                 .commitNow()
         }
+        registerReceiver(receiver, IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED))
     }
-//
-//    @RequiresApi(Build.VERSION_CODES.N)
-//    private var clickListener = View.OnClickListener {
-//        try {
-//            val uri = URL(binding.url.text.toString())
-//            val handler = Handler(Looper.getMainLooper()) // Запоминаем основной поток
-//            Thread {
-//                var urlConnection: HttpsURLConnection? = null
-//                try {
-//                    urlConnection = uri.openConnection() as HttpsURLConnection
-//                    urlConnection.requestMethod =
-//                        "GET" // установка метода получения данных -- GET
-//                    urlConnection.readTimeout = 10000 // установка таймаута 10 секунд
-//                    val reader =
-//                        BufferedReader(InputStreamReader(urlConnection.inputStream)) // Читаем данные в поток (бинар в стрим, в буффер)
-//                    val result = getLines(reader)
-//                    handler.post { // Возвращаемся к основному потоку
-//                        binding.webView.loadData(result, "text/html; charset=utf-8", "utf-8")
-//                    }
-//                } catch (e: Exception) {
-//                    Log.e("", "Fail connection", e)
-//                    e.printStackTrace()
-//                } finally {
-//                    urlConnection?.disconnect()
-//                }
-//            }.start()
-//        } catch (e: MalformedURLException) {
-//            Log.e("", "Fail URI", e)
-//            e.printStackTrace()
-//        }
-//    }
-//
-//    @RequiresApi(Build.VERSION_CODES.N)
-//    private fun getLines(reader: BufferedReader): String {
-//        return reader.lines().collect(Collectors.joining("\n"))
-//    }
 
-//    object clickListener: View.OnClickListener {
-//        var urlConnection: HttpsURLConnection? = null
-//        try {
-//            val uri = URL(binding.url.text.toString())
-//        }
-//    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_screen_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_threads -> {
+                supportFragmentManager.apply {
+                    beginTransaction()
+                        .replace(R.id.container, ThreadsFragment.newInstance())
+                        .addToBackStack("")
+                        .commitAllowingStateLoss()
+                }
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onDestroy() {
+        unregisterReceiver(receiver)
+        super.onDestroy()
+    }
 }
---
+++ app/src/main/java/molinov/weather/view/experiments/MainService.kt
@@ -0,0 +1,34 @@
+package molinov.weather.view.experiments
+
+import android.app.IntentService
+import android.content.Intent
+import android.util.Log
+
+private const val TAG = "MainServiceTAG"
+const val MAIN_SERVICE_STRING_EXTRA = "MainServiceExtra"
+
+class MainService(name: String = "MainService") : IntentService(name) {
+
+    override fun onHandleIntent(intent: Intent?) {
+        createLogMessage("onHandleIntent ${intent?.getStringExtra(MAIN_SERVICE_STRING_EXTRA)}")
+    }
+
+    override fun onCreate() {
+        createLogMessage("onCreate")
+        super.onCreate()
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        createLogMessage("onStartCommand")
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onDestroy() {
+        createLogMessage("onDestroy")
+        super.onDestroy()
+    }
+
+    private fun createLogMessage(message: String) {
+        Log.d(TAG, message)
+    }
+}
